df764208c33dcf52a7fe448a880cde2aa1a066a9
akash <akash.sukhija@gmail.com>
2020-05-03 15:00:04 -0500

    Changed the add_routes function to include randomization for alcohol and number of roads Changed the accidents_per_edge function

--- car_accident_mc.py
+++ car_accident_mc.py
@@ -6,59 +6,73 @@ random.seed(10)
 
 
 def add_routes(d):
-    car_stat = {"no_of_cars": 1000, "alcohol": 0}
+    # car_stat = {"no_of_cars" : 1000, "alcohol": 50}
     random.seed(10)
     counties = ['Adams', 'Alexander', 'Bond', 'Boone', 'Brown', 'Bureau', 'Calhoun', 'Carroll', 'Cass', 'Champaign',
-                'Christian', 'Clark', 'Clay', 'Clinton', 'Coles', 'Cook', 'Crawford', 'Cumberland', 'DeKalb', 'De Witt',
-                'Douglas', 'Dupage', 'Edgar', 'Edwards', 'Effingham', 'Fayette', 'Ford', 'Franklin', 'Fulton',
-                'Gallatin', 'Greene', 'Grundy', 'Hamilton', 'Hancock', 'Hardin', 'Henderson', 'Henry', 'Iroquois',
-                'Jackson', 'Jasper', 'Jefferson', 'Jersey', 'Jo Daviess', 'Johnson', 'Kane', 'Kankakee', 'Kendall',
-                'Knox', 'Lake', 'LaSalle', 'Lawrence', 'Lee', 'Livingston', 'Logan', 'Macon', 'Macoupin', 'Madison',
-                'Marion', 'Marshall', 'Mason', 'Massac', 'McDonough', 'McHenry', 'McLean', 'Menard', 'Mercer', 'Monroe',
-                'Montgomery', 'Morgan', 'Moultrie', 'Ogle', 'Peoria', 'Perry', 'Piatt', 'Pike', 'Pope', 'Pulaski',
-                'Putnam', 'Randolph', 'Richland', 'Rock Island', 'Saline', 'Sangamon', 'Schuyler', 'Scott', 'Shelby',
-                'St. Clair', 'Stark', 'Stephenson', 'Tazewell', 'Union', 'Vermilion', 'Wabash', 'Warren', 'Washington',
-                'Wayne', 'White', 'Whiteside', 'Will', 'Williamson', 'Winnebago', 'Woodford']
+                'Christian', 'Clark','Clay', 'Clinton', 'Coles', 'Cook', 'Crawford', 'Cumberland', 'DeKalb', 'De Witt', 'Douglas', 'Dupage',
+                'Edgar', 'Edwards', 'Effingham',
+                'Fayette', 'Ford', 'Franklin', 'Fulton', 'Gallatin', 'Greene', 'Grundy', 'Hamilton', 'Hancock',
+                'Hardin', 'Henderson', 'Henry', 'Iroquois', 'Jackson',
+                'Jasper', 'Jefferson', 'Jersey', 'Jo Daviess', 'Johnson', 'Kane', 'Kankakee', 'Kendall', 'Knox', 'Lake',
+                'LaSalle', 'Lawrence', 'Lee', 'Livingston', 'Logan',
+                'Macon', 'Macoupin', 'Madison', 'Marion', 'Marshall', 'Mason', 'Massac', 'McDonough', 'McHenry',
+                'McLean', 'Menard', 'Mercer', 'Monroe', 'Montgomery', 'Morgan',
+                'Moultrie', 'Ogle', 'Peoria', 'Perry', 'Piatt', 'Pike', 'Pope', 'Pulaski', 'Putnam', 'Randolph',
+                'Richland', 'Rock Island', 'Saline', 'Sangamon', 'Schuyler',
+                'Scott', 'Shelby', 'St. Clair', 'Stark', 'Stephenson', 'Tazewell', 'Union', 'Vermilion', 'Wabash',
+                'Warren', 'Washington', 'Wayne', 'White', 'Whiteside', 'Will',
+                'Williamson', 'Winnebago', 'Woodford']
     counties = counties[:36]
     # State Roads
     for i in range(1, len(counties)):
         distance = random.uniform(15, 25)
-        d.add_edge(counties[i], counties[i - 1], weight=distance, type="State", car_stat=car_stat)
-        d.add_edge(counties[i - 1], counties[i], weight=distance, type="State", car_stat=car_stat)
+        state_car_stat = {"alcohol": random.randint(0, 100) ,"no_of_cars": random.randint(1000, 2500)}
+        d.add_edge(counties[i], counties[i - 1], weight=distance, type="State", car_stat=state_car_stat)
+        d.add_edge(counties[i - 1], counties[i], weight=distance, type="State", car_stat=state_car_stat)
     distance = random.uniform(15, 25)
-    d.add_edge(counties[0], counties[i], weight=distance, type="State", car_stat=car_stat)
-    d.add_edge(counties[i], counties[0], weight=distance, type="State", car_stat=car_stat)
-
+    d.add_edge(counties[0], counties[i], weight=distance, type="State", car_stat=state_car_stat)
+    d.add_edge(counties[i], counties[0], weight=distance, type="State", car_stat=state_car_stat)
     # InterState Roads
     for i in range(1, 10):
         distance = random.uniform(75, 100)
-        d.add_edge(counties[i * 3], counties[i * 3 - 3], weight=distance, type="InterState", car_stat=car_stat)
-        d.add_edge(counties[(i * 3) - 3], counties[(i * 3)], weight=distance, type="InterState", car_stat=car_stat)
+        interstate_car_stat = {"alcohol": random.randint(50, 300),"no_of_cars": random.randint(1500, 5000)}
+        d.add_edge(counties[i * 3], counties[i * 3 - 3], weight=distance, type="InterState",
+                   car_stat=interstate_car_stat)
+        d.add_edge(counties[(i * 3) - 3], counties[(i * 3)], weight=distance, type="InterState",
+                   car_stat=interstate_car_stat)
     for i in range(1, 9):
         distance = random.uniform(100, 125)
-        d.add_edge(counties[i * 4], counties[(i * 4) - 4], weight=distance, type="InterState", car_stat=car_stat)
-        d.add_edge(counties[(i * 4) - 4], counties[(i * 4)], weight=distance, type="InterState", car_stat=car_stat)
+        d.add_edge(counties[i * 4], counties[(i * 4) - 4], weight=distance, type="InterState",
+                   car_stat=interstate_car_stat)
+        d.add_edge(counties[(i * 4) - 4], counties[(i * 4)], weight=distance, type="InterState",
+                   car_stat=interstate_car_stat)
 
     # City Roads
     for i in range(1, 7):
         distance = random.uniform(100, 125)
-        d.add_edge(counties[i * 5], counties[(i * 5) - 5 - i], weight=distance, type="city", car_stat=car_stat)
-        d.add_edge(counties[(i * 5) - 5 - i], counties[(i * 5)], weight=distance, type="city", car_stat=car_stat)
+        city_car_stat = { "alcohol": random.randint(0, 50), "no_of_cars": random.randint(500, 2000)}
+        d.add_edge(counties[i * 5], counties[(i * 5) - 5 - i], weight=distance, type="city", car_stat=city_car_stat)
+        d.add_edge(counties[(i * 5) - 5 - i], counties[(i * 5)], weight=distance, type="city", car_stat=city_car_stat)
     for i in range(1, 6):
         distance = random.uniform(150, 175)
-        d.add_edge(counties[i * 6], counties[(i * 6) - 6 - i], weight=distance, type="city", car_stat=car_stat)
-        d.add_edge(counties[(i * 6) - 6 - i], counties[(i * 6)], weight=distance, type="city", car_stat=car_stat)
+        d.add_edge(counties[i * 6], counties[(i * 6) - 6 - i], weight=distance, type="city", car_stat=city_car_stat)
+        d.add_edge(counties[(i * 6) - 6 - i], counties[(i * 6)], weight=distance, type="city", car_stat=city_car_stat)
     return d
 
+# First Function
 def initializemap():
   d = nx.DiGraph()
   #d = add_counties(d)
   d = add_routes(d)
   return d
 
+#Fifth Function
 def accidents_per_edge(edgestat):
-  return (np.random.binomial(edgestat['alcohol'],0.03)+np.random.binomial(edgestat['no_of_cars']-edgestat['alcohol'],0.27))
+  return (np.random.binomial(edgestat[next(iter(edgestat))],0.1) + np.random.binomial(edgestat['no_of_cars']-edgestat[next(iter(edgestat))],0.07))
+#Assumption: If there are 100 vehicles, 10 collisions happen then out of those 10 collisions, 3 are due to alchohol
+#and 7 due to other causes
 
+#Fourth Function
 def accidents_per_roadtype(d):
   cumulative_stat = {'State': 0,'InterState': 0 , 'City': 0}
   for i in range(0,len(d.edges(data=True))):
@@ -70,7 +84,7 @@ def accidents_per_roadtype(d):
       cumulative_stat['City'] += accidents_per_edge((list(d.edges(data=True))[i])[2]['car_stat'])
   print(cumulative_stat)
 
-
+#Main Function
 if __name__ == "__main__":
     d = initializemap()
 
