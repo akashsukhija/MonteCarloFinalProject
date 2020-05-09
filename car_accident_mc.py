e6b73aa9b8bda22ad2e3718ca0cda7536d778f95
siddharthshetty08 <siddharthshetty.08@gmail.com>
2020-05-09 13:55:44 -0500

    Added doc tests and strings.

--- car_accident_mc.py
+++ car_accident_mc.py
@@ -7,8 +7,18 @@ random.seed(10)
 
 
 def create_map(d, hypothesis):
-    # car_stat = {"no_of_cars" : 1000, "h_param": 50}
-    # State Roads
+    '''
+    The function is to create an if else ladder for iterating through different types of hypothesis parameters 'alcohol', 'distraction' and 'autonomous'.
+    There h_multiplier multiplies with the number of cars. The multiplier for alcohol is 10 and with distraction is 15 because according to statistics obtained
+    from 'Illinois Department of Transportation' and 'Illinois Tollway' and a few assumptions made for autonomous vehicles to reach a conclusion.
+
+    :param d: Represents the Digraph Created for Testing a Hypothesis
+    :param hypothesis: Hypothesis is the type of hypothesis under test (Alcohol, Distraction, Autonomous only)
+    :return: The function returns a network graph with the hypothesis multiplier value depending upon the hypothesis.
+    >>> d = nx.DiGraph()
+    >>> len(create_map(d,"alcohol"))
+    42
+    '''
     if hypothesis == "alcohol":
         h_multiplier = 10
     elif hypothesis == "distraction":
@@ -19,7 +29,19 @@ def create_map(d, hypothesis):
     return d
 
 def calculate_traffic(h_multiplier, roadtype):
-    car_stat = {}
+    '''
+    As part of this function, we assign traffic to each type of route and the number of cars satisfying the hypothesis parameter.
+    For an InterState Type Road we have assumed the maximum traffic, followed by State Type Road and then to the City Roads.
+    The cars satisfying the hypothesis parameter are also adjusted according to the type of road.
+
+    :param h_multiplier: The h_multiplier variable is chosen in the create_map function it may vary according to the hypothesis under test
+    :param roadtype: roadtype is variable which represents the different types of variables
+    :return: A dictionary called car_stat is returned by the function
+    >>> calculate_traffic(10,"State")
+    {'h_param': 0, 'autonomous': 21, 'no_of_cars': 122}
+    >>> calculate_traffic(0,"InterState")
+    {'h_param': 0, 'autonomous': 22, 'no_of_cars': 374}
+    '''
     if h_multiplier == 0 and roadtype == "State":
         car_stat = {"h_param": 0,"autonomous":random.randint(10, 25), "no_of_cars": random.randint(100, 250)}
     elif roadtype == "State":
@@ -35,6 +57,14 @@ def calculate_traffic(h_multiplier, roadtype):
     return car_stat
 
 def add_routes(d,h_multiplier):
+    """
+    :param d: Represents the Digraph Created for Testing a Hypothesis
+    :param h_multiplier: The h_multiplier variable is chosen in the create_map function it may vary according to the hypothesis under test
+    :return: Returns a network digraph with added edges and attributes.
+    >>> t = nx.DiGraph()
+    >>> len(add_routes(t,10))
+    42
+    """
     random.seed(10)
     counties = ['Adams', 'Alexander', 'Bond', 'Boone', 'Brown', 'Bureau', 'Calhoun', 'Carroll', 'Cass', 'Champaign',
                 'Christian', 'Clark', 'Clay', 'Clinton', 'Coles', 'Cook', 'Crawford', 'Cumberland', 'DeKalb', 'De Witt',
@@ -90,13 +120,34 @@ def add_routes(d,h_multiplier):
 
 
 def initializemap(hypothesis):
-  d = nx.DiGraph()
-  #d = add_counties(d)
-  d = create_map(d,hypothesis)
-  return d
+    '''
+    As part of the initialize map function we have used a Network DiGraph as our principle data structure to create
+    The edges the represent the following attributes types of routes, the amount of traffic, number of cars with the
+    hypothesis parameter. For example, cars in which drivers are under influence of alcohol or are distracted driving
+    or cars which are autonomous.
+
+    :param hypothesis: Hypothesis represents the hypothesis under test which may be 'alchohol' or any of the three hypothesis.
+    :return: The function returns a created map with all the routes added
+    '''
+    d = nx.DiGraph()
+    # d = add_counties(d)
+    d = create_map(d, hypothesis)
+    return d
 
 
 def accidents_per_edge(edgestat,hypothesis):
+    """
+    In this function we calculate the accidents happening per edge or per route depending upon the type of
+    hypothesis. The formula is based upon facts and figures obtained from 'Illinois Department of Transportation' and
+    'Illinois Tollway' and a few assumptions made for autonomous vehicles to reach the conclusion for the hypothesis.
+
+    :param edgestat: edgestat is a dictionary which is being passed, this dictionary is the output of the assign_traffic function
+    :param hypothesis:Hypothesis represents the hypothesis under test which may be 'alcohol' or any of the three hypothesis.
+    :return: The function returns a randomly generated number (samples from a parameterized binomial distribution)
+
+    >>> type(accidents_per_edge({'h_param': 9, 'autonomous': 11, 'no_of_cars': 209},'alcohol'))
+    <class 'int'>
+    """
     if hypothesis == "alcohol":
         return (np.random.binomial(edgestat[next(iter(edgestat))], 0.033) + np.random.binomial(edgestat['autonomous'],0.02) + np.random.binomial(edgestat['no_of_cars'] - edgestat['autonomous'] - edgestat[next(iter(edgestat))], 0.03))
     elif hypothesis == "distraction":
@@ -108,18 +159,31 @@ def accidents_per_edge(edgestat,hypothesis):
 #and 7 due to other causes
 
 def accidents_per_roadtype(d,hypothesis):
-  cumulative_stat = {'State': 0,'InterState': 0 , 'City': 0}
-  for i in range(0,len(d.edges(data=True))):
-    if list(d.edges(data=True))[i][2]['type'] == 'State':
-      cumulative_stat['State'] += accidents_per_edge((list(d.edges(data=True))[i])[2]['car_stat'],hypothesis)
-    elif list(d.edges(data=True))[i][2]['type'] == 'InterState':
-      cumulative_stat['InterState'] += accidents_per_edge((list(d.edges(data=True))[i])[2]['car_stat'],hypothesis)
-    else:
-      cumulative_stat['City'] += accidents_per_edge((list(d.edges(data=True))[i])[2]['car_stat'],hypothesis)
-  return(cumulative_stat)
+    """
+    In this function we have calculated and stored the total number of accidents happening per road type.
+
+    :param d: Represents the Digraph Created for Testing a Hypothesis
+    :param hypothesis: Hypothesis represents the hypothesis under test which may be 'alcohol' or any of the three hypothesis.
+    :return: All values of accidents are stored in a dictionary called cumulative_stat.
+    """
+    cumulative_stat = {'State': 0, 'InterState': 0, 'City': 0}
+    for i in range(0, len(d.edges(data=True))):
+        if list(d.edges(data=True))[i][2]['type'] == 'State':
+            cumulative_stat['State'] += accidents_per_edge((list(d.edges(data=True))[i])[2]['car_stat'], hypothesis)
+        elif list(d.edges(data=True))[i][2]['type'] == 'InterState':
+            cumulative_stat['InterState'] += accidents_per_edge((list(d.edges(data=True))[i])[2]['car_stat'],
+                                                                hypothesis)
+        else:
+            cumulative_stat['City'] += accidents_per_edge((list(d.edges(data=True))[i])[2]['car_stat'], hypothesis)
+    return (cumulative_stat)
 
 
 def route_display(d):
+    '''
+    In this function we are displaying or visualizing the networkx graph created for the hypothesis under test.
+    :param d: Represents the Digraph Created for Testing a Hypothesis
+    :return: The function returns no value but prints the networkx graph plot
+    '''
     my_pos = nx.spring_layout(d, seed=50)
     edge_color = []
     mapdict = (nx.get_edge_attributes(d, "type"))
@@ -135,7 +199,15 @@ def route_display(d):
     plt.show()
 
 def run_experiment(hypothesis):
-    data = {}
+    """
+    The run experiment function runs the hypothesis for a specific time period. In our case '182' represents the
+    number of days for which the experiment is running. These results are stored in a pandas dataframe in the
+    columns 'State', Interstate' and 'City'. The functions helps to calculate the minimum value, maximum value,
+    median and mean of the dataframe
+    :param hypothesis: hypothesis: Hypothesis represents the hypothesis under test which may be
+    'alcohol' or any of the three hypothesis.
+    :return: List consisting of total number of accidents.
+    """
     df_final = pd.DataFrame(columns=['State', 'InterState', 'City'])
 
     for i in range(365):
